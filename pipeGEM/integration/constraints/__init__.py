fb5f610272513cad2e36d8ec1aec7835d16635ca
qwerty239qwe <qwerty239qwe@gmail.com>
2021-11-11 19:05:04 -0600

    implemented GIMME and EFlux pipelines

--- pipeGEM/integration/constraints/__init__.py
+++ pipeGEM/integration/constraints/__init__.py
@@ -1,3 +1,5 @@
 from pipeGEM.integration.constraints._registering import *
 from pipeGEM.integration.constraints._utils import *
 from pipeGEM.integration.constraints._processing import *
+from pipeGEM.integration.constraints.Eflux import Eflux
+from pipeGEM.integration.constraints.GIMME import GIMME
--- pipeGEM/pipeline/algo/constraints.py
+++ pipeGEM/pipeline/algo/constraints.py
@@ -1,20 +1,43 @@
+import cobra
+
 from pipeGEM.pipeline import Pipeline
 from pipeGEM.pipeline.preprocessing import GeneDataSet
+from pipeGEM.integration import constraints
 
 
 class GIMME(Pipeline):
     def __init__(self, data_df):
         super(GIMME, self).__init__()
         self.gene_dataset = GeneDataSet(data_df)
+        self.data_df = data_df
 
-    def run(self, model, dataset):
-        raise NotImplementedError()
+    def run(self, model, **kwargs):
+        expr_dict = self.gene_dataset()
+
+        self.output = {}
+        for c in self.data_df.columns:
+            const_model = model.copy()
+            constraints.GIMME(const_model,
+                              rxn_expr_score=expr_dict[c].rxn_scores,
+                              **kwargs)
+            self.output[c] = const_model
+        return self.output
 
 
 class Eflux(Pipeline):
     def __init__(self, data_df):
         super(Eflux, self).__init__()
         self.gene_dataset = GeneDataSet(data_df)
+        self.data_df = data_df
 
-    def run(self, model, dataset):
-        raise NotImplementedError()
+    def run(self, model: cobra.Model, **kwargs):
+        expr_dict = self.gene_dataset()
+
+        self.output = {}
+        for c in self.data_df.columns:
+            const_model = model.copy()
+            constraints.Eflux(const_model,
+                              rxn_expr_score=expr_dict[c].rxn_scores,
+                              **kwargs)
+            self.output[c] = const_model
+        return self.output
