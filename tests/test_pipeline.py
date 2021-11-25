392a31be68b65ea3364ae4a8004f1ce227820658
yu-de lin <qwerty239qwe@gmail.com>
2021-11-25 17:34:59 +0800

    change tested model

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,10 +1,10 @@
 from pipeGEM.pipeline.algo import SwiftCC
 
 
-def test_swiftcc(ecoli):
+def test_swiftcc(ecoli_core):
     s = SwiftCC()
-    consist = s(ecoli)
-    assert len(consist["model"].reactions) > 0, len(consist["model"].reactions) != len(ecoli.reactions)
-    assert len(consist["model"].metabolites) > 0, len(consist["model"].metabolites) != len(ecoli.metabolites)
-    assert len(consist["model"].genes) > 0, len(consist["model"].genes) != len(ecoli.genes)
+    consist = s(ecoli_core)
+    assert len(consist["model"].reactions) > 0, len(consist["model"].reactions) != len(ecoli_core.reactions)
+    assert len(consist["model"].metabolites) > 0, len(consist["model"].metabolites) != len(ecoli_core.metabolites)
+    assert len(consist["model"].genes) > 0, len(consist["model"].genes) != len(ecoli_core.genes)
 
