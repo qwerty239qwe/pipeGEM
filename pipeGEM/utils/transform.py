bfc1cd285bb27b5f47b1bc48cfcd76401ccd1942
qwerty239qwe <qwerty239qwe@gmail.com>
2022-12-22 13:38:54 +0800

    change the default coefs

--- pipeGEM/utils/transform.py
+++ pipeGEM/utils/transform.py
@@ -5,10 +5,10 @@ def sigmoid(x, coef):
     return 1 / (1 + np.exp(-coef * x))
 
 
-def mod_log10(x, coef=1) -> float:
+def mod_log10(x, coef=1001) -> float:
     return np.log10(x + coef)
 
 
-def exp_x(x, coef=1, a=1.5):
+def exp_x(x, coef=1, a=1.1):
     return a ** x - coef
 
